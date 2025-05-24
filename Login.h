e7d11a6451585f6ab4a8758e95b5e8ea177759c6
고여경 <2yokyung@naver.com>
2025-05-25 03:53:02 +0900

    feat: Login 기능

---
+++ Login.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <utility>
+
+class Login {
+    public:
+        static bool login(const std::string& id, const std::string& password);
+
+        static bool isLogin();
+};
